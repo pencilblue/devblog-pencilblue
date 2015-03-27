ff1f1cdd3b81d489545e79c15e7f05569ce21373
Blake Callens <blake@appsqa-dmza.bcbsnc.com>
2015-03-27 14:35:59 -0400

    upgrade to 0.4.0

--- controllers/home_page_settings.js
+++ controllers/home_page_settings.js
@@ -1,105 +1,108 @@
-/**
- * HomePageSettings - Settings for the display of home page content in the Portfolio theme
- *
- * @author Blake Callens <blake@pencilblue.org>
- * @copyright 2014 PencilBlue, LLC.  All Rights Reserved
- */
+module.exports = function HomePageSettingsModule(pb) {
+  /**
+   * HomePageSettings - Settings for the display of home page content in the Portfolio theme
+   *
+   * @author Blake Callens <blake@pencilblue.org>
+   * @copyright 2014 PencilBlue, LLC.  All Rights Reserved
+   */
 
-function HomePageSettings() {}
+  function HomePageSettings() {}
 
-//dependencies
-var PluginService = pb.PluginService;
+  //dependencies
+  var PluginService = pb.PluginService;
+  var util = pb.util;
 
-//inheritance
-util.inherits(HomePageSettings, pb.BaseController);
+  //inheritance
+  util.inherits(HomePageSettings, pb.BaseController);
 
-HomePageSettings.prototype.render = function(cb) {
-    var self = this;
+  HomePageSettings.prototype.render = function(cb) {
+      var self = this;
 
-    var content = {
-        content_type: "text/html",
-        code: 200
-    };
+      var content = {
+          content_type: "text/html",
+          code: 200
+      };
 
-    var tabs = [
-        {
-            active: 'active',
-            href: '#home_layout',
-            icon: 'home',
-            title: self.ls.get('HOME_LAYOUT')
-        },
-        {
-            href: '#media',
-            icon: 'picture-o',
-            title: self.ls.get('HOME_MEDIA')
-        }
-    ];
+      var tabs = [
+          {
+              active: 'active',
+              href: '#home_layout',
+              icon: 'home',
+              title: self.ls.get('HOME_LAYOUT')
+          },
+          {
+              href: '#media',
+              icon: 'picture-o',
+              title: self.ls.get('HOME_MEDIA')
+          }
+      ];
 
-    var pills = [
-    {
-        name: 'devblog_settings',
-        title: self.ls.get('HOME_PAGE_SETTINGS'),
-        icon: 'chevron-left',
-        href: '/admin/plugins/devblog-pencilblue/settings'
-    }];
+      var pills = [
+      {
+          name: 'devblog_settings',
+          title: self.ls.get('HOME_PAGE_SETTINGS'),
+          icon: 'chevron-left',
+          href: '/admin/plugins/devblog-pencilblue/settings'
+      }];
 
-    var opts = {
-        where: {settings_type: 'home_page'}
-    };
-    var dao  = new pb.DAO();
-    dao.q('devblog_theme_settings', opts, function(err, homePageSettings) {
-        if(homePageSettings.length > 0) {
-            homePageSettings = homePageSettings[0];
-        }
-        else {
-            homePageSettings = {callouts: [{}, {}, {}]};
-        }
+      var opts = {
+          where: {settings_type: 'home_page'}
+      };
+      var dao  = new pb.DAO();
+      dao.q('devblog_theme_settings', opts, function(err, homePageSettings) {
+          if(homePageSettings.length > 0) {
+              homePageSettings = homePageSettings[0];
+          }
+          else {
+              homePageSettings = {callouts: [{}, {}, {}]};
+          }
 
-        var mservice = new pb.MediaService();
-        mservice.get(function(err, media) {
-            if(homePageSettings.page_media) {
-                var pageMedia = [];
-                for(i = 0; i < homePageSettings.page_media.length; i++) {
-                    for(j = 0; j < media.length; j++) {
-                        if(media[j]._id.equals(ObjectID(homePageSettings.page_media[i]))) {
-                            pageMedia.push(media[j]);
-                            media.splice(j, 1);
-                            break;
-                        }
-                    }
-                }
-                homePageSettings.page_media = pageMedia;
-            }
+          var mservice = new pb.MediaService();
+          mservice.get(function(err, media) {
+              if(homePageSettings.page_media) {
+                  var pageMedia = [];
+                  for(i = 0; i < homePageSettings.page_media.length; i++) {
+                      for(j = 0; j < media.length; j++) {
+                          if(media[j]._id.equals(ObjectID(homePageSettings.page_media[i]))) {
+                              pageMedia.push(media[j]);
+                              media.splice(j, 1);
+                              break;
+                          }
+                      }
+                  }
+                  homePageSettings.page_media = pageMedia;
+              }
 
-            var objects = {
-                navigation: pb.AdminNavigation.get(self.session, ['plugins', 'manage'], self.ls),
-                pills: pills,
-                tabs: tabs,
-                media: media,
-                homePageSettings: homePageSettings
-            };
+              var objects = {
+                  navigation: pb.AdminNavigation.get(self.session, ['plugins', 'manage'], self.ls),
+                  pills: pills,
+                  tabs: tabs,
+                  media: media,
+                  homePageSettings: homePageSettings
+              };
 
-            self.ts.registerLocal('angular_script', '');
-            self.ts.registerLocal('angular_objects', new pb.TemplateValue(pb.js.getAngularObjects(objects), false));
-            self.ts.load('admin/settings/home_page_settings', function(err, result) {
-                cb({content: result});
-            });
-        });
-    });
-};
+              self.ts.registerLocal('angular_script', '');
+              self.ts.registerLocal('angular_objects', new pb.TemplateValue(pb.ClientJs.getAngularObjects(objects), false));
+              self.ts.load('admin/settings/home_page_settings', function(err, result) {
+                  cb({content: result});
+              });
+          });
+      });
+  };
 
-HomePageSettings.getRoutes = function(cb) {
-    var routes = [
-    {
-        method: 'get',
-        path: '/admin/plugins/devblog/settings/home_page',
-        auth_required: true,
-        access_level: ACCESS_EDITOR,
-        content_type: 'text/html'
-    }
-    ];
-    cb(null, routes);
-};
+  HomePageSettings.getRoutes = function(cb) {
+      var routes = [
+      {
+          method: 'get',
+          path: '/admin/plugins/devblog/settings/home_page',
+          auth_required: true,
+          access_level: pb.SecurityService.ACCESS_EDITOR,
+          content_type: 'text/html'
+      }
+      ];
+      cb(null, routes);
+  };
 
-//exports
-module.exports = HomePageSettings;
+  //exports
+  return HomePageSettings;
+}
--- services/external_profiles.js
+++ services/external_profiles.js
@@ -1,91 +1,96 @@
-/**
- * ExternalProfilesService - An example of a service that generates random text.
- *
- * @author Brian Hyder <brian@pencilblue.org>
- * @copyright 2014 PencilBlue, LLC.  All Rights Reserved
- */
-function ExternalProfilesService() {}
+module.exports = function ExternalProfilesServiceModule(pb) {
+  /**
+   * ExternalProfilesService - An example of a service that generates random text.
+   *
+   * @author Brian Hyder <brian@pencilblue.org>
+   * @copyright 2014 PencilBlue, LLC.  All Rights Reserved
+   */
+  function ExternalProfilesService() {}
 
-/**
- * This function is called when the service is being setup by the system.  It is
- * responsible for any setup that is needed when first created.  The services
- * are all instantiated at once and are not added to the platform untill all
- * initialization is complete.  Relying on other plugin services in the
- * initialization could result in failure.
- *
- * @param cb A callback that should provide one argument: cb(error) or cb(null)
- * if initialization proceeded successfully.
- */
-ExternalProfilesService.init = function(cb) {
-    pb.log.debug("ExternalProfilesService: Initialized");
-    cb(null, true);
-};
+  // dependencies
+  var pluginService = new pb.PluginService();
 
-/**
- * Returns the profiles that are filled out in plugin settings
- */
-ExternalProfilesService.getProfiles = function(cb) {
-    var profiles = [];
-    var networks = [{
-        name: 'github_id',
-        displayName: 'Github',
-        icon: 'github',
-        color: '#829AA8',
-        prefix: 'https://github.com/'
-    }, {
-        name: 'twitter_id',
-        displayName: 'Twitter',
-        icon: 'twitter',
-        color: '#00B0ED',
-        prefix: 'https://twitter.com/'
-    }, {
-        name: 'stack_exchange_id',
-        displayName: 'StackExchange',
-        icon: 'stack-exchange',
-        color: '#195398',
-        prefix: 'http://stackexchange.com/users/'
-    }, {
-        name: 'stack_overflow_id',
-        displayName: 'StackOverflow',
-        icon: 'stack-overflow',
-        color: '#D38B28',
-        prefix: 'http://stackoverflow.com/users/'
-    }, {
-        name: 'linkedin_profile',
-        displayName: 'Linkedin',
-        icon: 'linkedin',
-        color: '#1D87BD',
-        prefix: ''
-    }, {
-        name: 'angellist_id',
-        displayName: 'AngelList',
-        icon: 'angellist',
-        color: '#333333',
-        prefix: 'https://angel.co/'
-    }, {
-        name: 'youtube_channel',
-        displayName: 'YouTube',
-        icon: 'youtube',
-        color: '#D92625',
-        prefix: ''
-    }];
+  /**
+   * This function is called when the service is being setup by the system.  It is
+   * responsible for any setup that is needed when first created.  The services
+   * are all instantiated at once and are not added to the platform untill all
+   * initialization is complete.  Relying on other plugin services in the
+   * initialization could result in failure.
+   *
+   * @param cb A callback that should provide one argument: cb(error) or cb(null)
+   * if initialization proceeded successfully.
+   */
+  ExternalProfilesService.init = function(cb) {
+      pb.log.debug("ExternalProfilesService: Initialized");
+      cb(null, true);
+  };
 
-    pb.plugins.getSettings('devblog-pencilblue', function(err, devblogSettings) {
-        for(var i = 0; i < networks.length; i++) {
-            for(var j = 0; j < devblogSettings.length; j++) {
-                if(devblogSettings[j].name === networks[i].name) {
-                    if(devblogSettings[j].value.length) {
-                        networks[i].value = devblogSettings[j].value;
-                        profiles.push(networks[i]);
-                    }
-                    break;
-                }
-            }
-        }
+  /**
+   * Returns the profiles that are filled out in plugin settings
+   */
+  ExternalProfilesService.getProfiles = function(cb) {
+      var profiles = [];
+      var networks = [{
+          name: 'github_id',
+          displayName: 'Github',
+          icon: 'github',
+          color: '#829AA8',
+          prefix: 'https://github.com/'
+      }, {
+          name: 'twitter_id',
+          displayName: 'Twitter',
+          icon: 'twitter',
+          color: '#00B0ED',
+          prefix: 'https://twitter.com/'
+      }, {
+          name: 'stack_exchange_id',
+          displayName: 'StackExchange',
+          icon: 'stack-exchange',
+          color: '#195398',
+          prefix: 'http://stackexchange.com/users/'
+      }, {
+          name: 'stack_overflow_id',
+          displayName: 'StackOverflow',
+          icon: 'stack-overflow',
+          color: '#D38B28',
+          prefix: 'http://stackoverflow.com/users/'
+      }, {
+          name: 'linkedin_profile',
+          displayName: 'Linkedin',
+          icon: 'linkedin',
+          color: '#1D87BD',
+          prefix: ''
+      }, {
+          name: 'angellist_id',
+          displayName: 'AngelList',
+          icon: 'angellist',
+          color: '#333333',
+          prefix: 'https://angel.co/'
+      }, {
+          name: 'youtube_channel',
+          displayName: 'YouTube',
+          icon: 'youtube',
+          color: '#D92625',
+          prefix: ''
+      }];
 
-        cb(null, profiles);
-    });
-};
+      pluginService.getSettings('devblog-pencilblue', function(err, devblogSettings) {
+          for(var i = 0; i < networks.length; i++) {
+              for(var j = 0; j < devblogSettings.length; j++) {
+                  if(devblogSettings[j].name === networks[i].name) {
+                      if(devblogSettings[j].value.length) {
+                          networks[i].value = devblogSettings[j].value;
+                          profiles.push(networks[i]);
+                      }
+                      break;
+                  }
+              }
+          }
 
-//exports
-module.exports = ExternalProfilesService;
+          cb(null, profiles);
+      });
+  };
+
+  //exports
+  return ExternalProfilesService;
+}
